86f866d249811c66662f1d96b1be22d32a54a920
Dumbuli21 <dumbuli2103@gmail.com>
2026-07-09 12:41:56 +0300

    Trust Railway proxy (isolated fix)

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,7 +11,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        // No custom middleware needed
+        $middleware->trustProxies(at: '*');
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
